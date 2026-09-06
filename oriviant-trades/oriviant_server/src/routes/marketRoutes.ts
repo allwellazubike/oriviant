4e3d96edf0c89e811a6a618157f6edb3dc26f51f
Maduka Jesse <madukajesse14@gmail.com>
2026-09-06 12:31:05 +0100

    chore: clean up accidental nested folders and stray root files

--- oriviant-trades/oriviant_server/src/routes/marketRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getMarketPrices, getQuote } from '../controllers/marketController.js';
-
-const router = Router();
-
-router.get('/prices', getMarketPrices);
-router.get('/quote', getQuote);
-
-export default router;
