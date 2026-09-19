6498e23e41a334879e414517c97b963590c87e37
Maduka Jesse Nnamdi <140384893+Jesse-23@users.noreply.github.com>
2026-09-19 14:59:11 +0100

    UI: remove Install App button from welcome view (#52)

--- oriviant-trades/dev-dist/sw.js
+++ oriviant-trades/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fsbu86o7sh8"
+    "revision": "0.qfah8tkp9m8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
